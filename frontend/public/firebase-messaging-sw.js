240dcef8be13842659aabcf92e3c492658238687
Shivansh7102003 <shivansh7102003@gmail.com>
2026-05-18 00:09:53 +0530

    feat(4-04-D): add FCM background service worker

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "analysts-dugout.firebaseapp.com",
+  projectId: "analysts-dugout",
+  storageBucket: "analysts-dugout.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.svg'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
